351d8a65f82a1a00fa495a281459a4c96685e2bc
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-11-09 20:27:42 +0700

    train yolov8 with VISEM training dataset

--- source_code/code_and_checkpoints/run_detect.sh
+++ source_code/code_and_checkpoints/run_detect.sh
@@ -4,13 +4,13 @@
 #SBATCH --nodelist=selab2
 #SBATCH --mem-per-cpu=4GB
 eval "$(conda shell.bash hook)"
-conda activate medico
+conda activate mediaeval
 
 # Yolov7 detection
 CUDA_LAUNCH_BLOCKING=1
 python ./yolov7/detect.py \
 --img-size 640 \
---source ./data/VISEM-Tracking/VISEM_Tracking_Train_v4/Train/11/11.mp4 \
+--source ./dataset/VISEM-Tracking/VISEM_Tracking_Train_v4/Train/11/11.mp4 \
 --weights ./models/yolov7.pt \
 --project ../../predictions/ \
 --exist-ok --save-txt \
--- source_code/code_and_checkpoints/run_track.sh
+++ source_code/code_and_checkpoints/run_track.sh
@@ -4,13 +4,13 @@
 #SBATCH --nodelist=selab2
 #SBATCH --mem-per-cpu=4GB
 eval "$(conda shell.bash hook)"
-conda activate medico
+conda activate mediaeval
 
 # SORT tracking
 CUDA_LAUNCH_BLOCKING=1
 python ./track.py \
 --img-size 640 \
---source ./data/VISEM-Tracking/VISEM_Tracking_Train_v4/Train/11/11.mp4 \
+--source ./dataset/VISEM-Tracking/VISEM_Tracking_Train_v4/Train/11/11.mp4 \
 --yolo-weights ./models/yolov7.pt \
 --project ../../predictions/ \
 --exist-ok --save-txt --save-vid
