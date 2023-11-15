cc984cf2653a9f495cddc8c5455b7473a6499878
Nguyen Van Loc <vanloc1808@gmail.com>
2023-11-15 09:17:35 +0000

    Implement print FPS and FLOPS function

--- source_code/code_and_checkpoints/yolov8/utils.py
+++ source_code/code_and_checkpoints/yolov8/utils.py
@@ -1,6 +1,9 @@
 import io
 import json
 import moviepy.editor as mp
+import torch
+import time
+from ultralytics import YOLO
 
 try:
     to_unicode = unicode
@@ -63,3 +66,24 @@ def convert_avi_to_mp4(avi_video_path: str, mp4_dest_video_path: str):
         )
     except OSError as e:
         raise OSError(f"Error occurred during conversion: {e}")
+
+
+def print_fps_and_flops(model: YOLO):
+    """Print FPS and FLOPS."""
+    input_tensor = torch.randn(1, 3, 224, 224)
+    # Measure the FPS
+    num_runs = 3
+    total_time = 0
+    for i in range(num_runs):
+        start_time = time.perf_counter()
+        with torch.no_grad():
+            output = model(input_tensor)
+        end_time = time.perf_counter()
+        total_time += end_time - start_time
+    fps = 1 / (total_time / num_runs)
+
+    # Measure the FLOPS
+    flops = torch.cuda.memory_allocated() * 2 / (total_time / num_runs)
+
+    print(f"FPS: {fps}")
+    print(f"FLOPS: {flops}")
