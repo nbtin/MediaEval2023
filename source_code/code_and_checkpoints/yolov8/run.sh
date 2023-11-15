d0a15a0844a058ab7911f7a80173e8fa8f302de9
Nguyen Van Loc <vanloc1808@gmail.com>
2023-11-15 07:20:33 +0000

    Update run.sh

--- source_code/code_and_checkpoints/yolov8/run.sh
+++ source_code/code_and_checkpoints/yolov8/run.sh
@@ -10,5 +10,31 @@ CUDA_LAUNCH_BLOCKING=1
 
 # pip install -r requirements.txt
 python3 run.py \
-    --weights=runs/detect/train3/weights/best.pt \
-    --source=datasets/Test/66.mp4
+    --weights=runs/detect/train7/weights/best.pt \
+    --source=datasets/Test/66.mp4 \
+    --save-txt \
+    --save-json
+
+python3 run.py \
+    --weights=runs/detect/train7/weights/best.pt \
+    --source=datasets/Test/68.mp4 \
+    --save-txt \
+    --save-json
+
+python3 run.py \
+    --weights=runs/detect/train7/weights/best.pt \
+    --source=datasets/Test/73.mp4 \
+    --save-txt \
+    --save-json
+
+python3 run.py \
+    --weights=runs/detect/train7/weights/best.pt \
+    --source=datasets/Test/76.mp4 \
+    --save-txt \
+    --save-json
+
+python3 run.py \
+    --weights=runs/detect/train7/weights/best.pt \
+    --source=datasets/Test/80.mp4 \
+    --save-txt \
+    --save-json
